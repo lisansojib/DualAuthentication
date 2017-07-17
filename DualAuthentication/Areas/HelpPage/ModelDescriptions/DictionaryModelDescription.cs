0d7a27d65d99a3e938c8b16714accdf414599fbb
lisansojib <lisancseru@gmail.com>
2017-07-17 11:35:59 +0600

    Initial Commit

---
+++ DualAuthentication/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace DualAuthentication.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ DualAuthentication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DualAuthentication.WebApiApplication" Language="C#" %>
